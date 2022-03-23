76ddb2c11aa597a3dae495531efc94927bf5e1c9
Philipp Bächler <philipp.baechler@gmail.com>
2022-03-23 19:55:39 +0100

    Refactoring

--- apple/generate_single_table.py
+++ apple/generate_single_table.py
@@ -16,68 +16,56 @@ get_day_of_week = lambda x: convert_tz(x).weekday()
 
 
 # %%
-def convert_to_date_time_append_to_df(df, field):
-    df[field] = pd.to_datetime(df[field])
-    df['year'] = df[field].map(get_year)
-    df['month'] = df[field].map(get_month)
-    df['date'] = df[field].map(get_date)
-    df['day'] = df[field].map(get_day)
-    df['hour'] = df[field].map(get_hour)
-    df['dow'] = df[field].map(get_day_of_week)
-    return df
-
-
 def convert_to_date_time_append_to_df(df):
-    df["creationDate"] = pd.to_datetime(df["creationDate"])
-    df["startDate"] = pd.to_datetime(df["startDate"])
-    df["endDate"] = pd.to_datetime(df["endDate"])
-    df["start_date"] = df["startDate"].map(get_date)
-    df["creation_date"] = df["startDate"].map(get_date)
+    df["creationDateTime"] = pd.to_datetime(df["creationDate"])
+    df["startDateTime"] = pd.to_datetime(df["startDate"])
+    df["endDateTime"] = pd.to_datetime(df["endDate"])
+    df = df.drop(columns=["creationDate", "startDate", "endDate"])
+    df["start_date"] = df["startDateTime"].map(get_date)
+    df["creation_date"] = df["creationDateTime"].map(get_date)
     return df
+
 
 # %% steps
 steps = pd.read_csv("data/StepCount.csv")
-steps = convert_to_date_time_append_to_df(steps, "startDate")
+steps = convert_to_date_time_append_to_df(steps)
 steps.sample(5)
 # %%
-steps_by_date = steps.groupby(["date"])["value"].sum().reset_index(name="Steps")
+steps_by_date = steps.groupby(["start_date"])["value"].sum().reset_index(name="Steps")
 print(steps_by_date.head())
 print(steps_by_date.shape)
 
 
 # %% restingHR
 restingHR = pd.read_csv("data/RestingHeartRate.csv")
-restingHR = convert_to_date_time_append_to_df(restingHR, "startDate")
+restingHR = convert_to_date_time_append_to_df(restingHR)
 restingHR.sample(5)
 # %%
 # find double values
-print("Unique Dates:", len(restingHR["date"].unique()))
+print("Unique Dates:", len(restingHR["start_date"].unique()))
 print("Number of entries:", restingHR.shape[0])
-restingHR.groupby(["date"]).count().sort_values(by="startDate")
+restingHR.groupby(["start_date"]).count().sort_values(by="startDateTime")
 # %%
 # replace with mean of the day
-restingHR_by_day = restingHR.groupby(["date"])["value"].mean().reset_index(name="Steps")
+restingHR_by_day = restingHR.groupby(["start_date"])["value"].mean().reset_index(name="rhr")
 print(restingHR_by_day.head())
 print(restingHR_by_day.shape)
 
 
 # %% vo2max
 vo2max = pd.read_csv("data/VO2Max.csv")
-vo2max = convert_to_date_time_append_to_df(vo2max, "startDate")
+vo2max = convert_to_date_time_append_to_df(vo2max)
 vo2max.sample(5)
 # %% 
 # replace with mean
-vo2max_by_day = vo2max.groupby(["date"])["value"].mean().reset_index(name="Steps")
+vo2max_by_day = vo2max.groupby(["start_date"])["value"].mean().reset_index(name="vo2max")
 print(vo2max_by_day.head())
 print(vo2max_by_day.shape)
 
 
 # %% sleep
 sleep = pd.read_csv("data/SleepAnalysis.csv")
-sleep["startDate"] = pd.to_datetime(sleep["startDate"])
-sleep["endDate"] = pd.to_datetime(sleep["endDate"])
-sleep["creationDate"] = pd.to_datetime(sleep["creationDate"])
-sleep["date"] = sleep["startDate"].map(get_date)
+sleep = convert_to_date_time_append_to_df(sleep)
 sleep.sample(5)
 # %% how many of each data source?
 sleep.groupby(["sourceName"]).count()
@@ -85,19 +73,19 @@ sleep.groupby(["sourceName"]).count()
 # pay attention to the special character between Apple and Watch (U+00a0)  -> maybe use AutoSleep / Apple Watch von Laura
 sleep_data = sleep.loc[sleep["sourceName"]=="AutoSleep"]
 sleep_data = sleep_data.loc[sleep_data["value"]=="HKCategoryValueSleepAnalysisAsleep"]
-sleep_data['time_asleep'] = sleep_data['endDate'] - sleep_data['startDate']
+sleep_data['time_asleep'] = sleep_data['endDateTime'] - sleep_data['startDateTime']
 sleep_data.head()
 #%%
-sleep_data = sleep_data.groupby('creationDate').agg(total_time_asleep=('time_asleep', 'sum'),
-    bed_time=('startDate', 'min'), 
-    awake_time=('endDate', 'max'), 
-    sleep_counts=('creationDate','count'))
+sleep_data = sleep_data.groupby('creationDateTime').agg(total_time_asleep=('time_asleep', 'sum'),
+    bed_time=('startDateTime', 'min'), 
+    awake_time=('endDateTime', 'max'), 
+    sleep_counts=('creationDateTime','count'))
 # %%
 sleep_data['time_in_bed'] = sleep_data['awake_time'] - sleep_data['bed_time']
 sleep_data['restless_time'] = sleep_data['time_in_bed'] - sleep_data['total_time_asleep']
 sleep_data = sleep_data.reset_index()
-sleep_data["date"] = sleep_data["creationDate"].map(get_date)
-sleep_data.head()
+print(sleep_data.head())
+print(sleep_data.shape)
 # %% plot
 sleep_data['time_in_bed'] = (sleep_data['time_in_bed'].dt.total_seconds()/60)
 sleep_data['total_time_asleep'] = (sleep_data['total_time_asleep'].dt.total_seconds()/60)
@@ -107,22 +95,24 @@ plt.show()
 
 # %% active calories burned
 activeEnergy = pd.read_csv("data/ActiveEnergyBurned.csv")
-activeEnergy = convert_to_date_time_append_to_df(activeEnergy, "startDate")
+activeEnergy = convert_to_date_time_append_to_df(activeEnergy)
 activeEnergy.sample(5)
 # %%
-activeEnergy_by_date = activeEnergy.groupby(["date"])["value"].sum().reset_index(name="activeEnergy")
-activeEnergy_by_date.head()
+activeEnergy_by_date = activeEnergy.groupby(["start_date"])["value"].sum().reset_index(name="activeEnergy")
+print(activeEnergy_by_date.head())
+print(activeEnergy_by_date.shape)
 
 # %% basal calories burned
 basalEnergy = pd.read_csv("data/BasalEnergyBurned.csv")
-basalEnergy = convert_to_date_time_append_to_df(basalEnergy, "startDate")
+basalEnergy = convert_to_date_time_append_to_df(basalEnergy)
 basalEnergy.sample(5)
 # %%
-basalEnergy_by_date = basalEnergy.groupby(["date"])["value"].sum().reset_index(name="basalEnergy")
-basalEnergy_by_date.head()
+basalEnergy_by_date = basalEnergy.groupby(["start_date"])["value"].sum().reset_index(name="basalEnergy")
+print(basalEnergy_by_date.head())
+print(basalEnergy_by_date.shape)
 
 
-# %% active calories burned
+# %% respiratory rate
 respiratoryRate = pd.read_csv("data/RespiratoryRate.csv")
 respiratoryRate = convert_to_date_time_append_to_df(respiratoryRate)
 respiratoryRate.sample(5)
@@ -133,9 +123,12 @@ respiratoryRate_by_date = respiratoryRate.groupby("creation_date").agg(
     respiratoryRate_min=("value", "min")
 ).reset_index()
 
-respiratoryRate_by_date.head()
+print(respiratoryRate_by_date.head())
+print(respiratoryRate_by_date.shape)
 # %%
 respiratoryRate_by_date.set_index("creation_date")[["respiratoryRate_mean", "respiratoryRate_max", "respiratoryRate_min"]].plot()
+
+
 # %%
 
 
