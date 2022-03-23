ba52a6662676c64e22e9b5b2d9a69bc8f15cb55f
Philipp Bächler <philipp.baechler@gmail.com>
2022-03-23 20:56:05 +0100

    Merge data frames to a single table

--- apple/generate_single_table.py
+++ apple/generate_single_table.py
@@ -31,7 +31,9 @@ steps = pd.read_csv("data/StepCount.csv")
 steps = convert_to_date_time_append_to_df(steps)
 steps.sample(5)
 # %%
-steps_by_date = steps.groupby(["start_date"])["value"].sum().reset_index(name="Steps")
+steps = steps.loc[steps["sourceName"]=="Apple Watch von Laura"]
+steps_by_date = steps.groupby(["start_date"])["value"].sum().reset_index(name="steps")
+steps_by_date = steps_by_date.rename(columns={"start_date": "date"})
 print(steps_by_date.head())
 print(steps_by_date.shape)
 
@@ -48,6 +50,7 @@ restingHR.groupby(["start_date"]).count().sort_values(by="startDateTime")
 # %%
 # replace with mean of the day
 restingHR_by_day = restingHR.groupby(["start_date"])["value"].mean().reset_index(name="rhr")
+restingHR_by_day = restingHR_by_day.rename(columns={"start_date": "date"})
 print(restingHR_by_day.head())
 print(restingHR_by_day.shape)
 
@@ -59,6 +62,7 @@ vo2max.sample(5)
 # %% 
 # replace with mean
 vo2max_by_day = vo2max.groupby(["start_date"])["value"].mean().reset_index(name="vo2max")
+vo2max_by_day = vo2max_by_day.rename(columns={"start_date": "date"})
 print(vo2max_by_day.head())
 print(vo2max_by_day.shape)
 
@@ -76,20 +80,22 @@ sleep_data = sleep_data.loc[sleep_data["value"]=="HKCategoryValueSleepAnalysisAs
 sleep_data['time_asleep'] = sleep_data['endDateTime'] - sleep_data['startDateTime']
 sleep_data.head()
 #%%
-sleep_data = sleep_data.groupby('creationDateTime').agg(total_time_asleep=('time_asleep', 'sum'),
+sleep_data_by_day = sleep_data.groupby('creationDateTime').agg(total_time_asleep=('time_asleep', 'sum'),
     bed_time=('startDateTime', 'min'), 
     awake_time=('endDateTime', 'max'), 
     sleep_counts=('creationDateTime','count'))
 # %%
-sleep_data['time_in_bed'] = sleep_data['awake_time'] - sleep_data['bed_time']
-sleep_data['restless_time'] = sleep_data['time_in_bed'] - sleep_data['total_time_asleep']
-sleep_data = sleep_data.reset_index()
-print(sleep_data.head())
-print(sleep_data.shape)
+sleep_data_by_day['time_in_bed'] = sleep_data_by_day['awake_time'] - sleep_data_by_day['bed_time']
+sleep_data_by_day['restless_time'] = sleep_data_by_day['time_in_bed'] - sleep_data_by_day['total_time_asleep']
+sleep_data_by_day = sleep_data_by_day.reset_index()
+sleep_data_by_day["date"] = sleep_data_by_day["creationDateTime"].map(get_date)
+sleep_data_by_day = sleep_data_by_day.drop(columns=["creationDateTime"])
+print(sleep_data_by_day.head())
+print(sleep_data_by_day.shape)
 # %% plot
-sleep_data['time_in_bed'] = (sleep_data['time_in_bed'].dt.total_seconds()/60)
-sleep_data['total_time_asleep'] = (sleep_data['total_time_asleep'].dt.total_seconds()/60)
-sleep_data[['time_in_bed','total_time_asleep']].plot(use_index=True)
+sleep_data_by_day['time_in_bed'] = (sleep_data_by_day['time_in_bed'].dt.total_seconds()/60)
+sleep_data_by_day['total_time_asleep'] = (sleep_data_by_day['total_time_asleep'].dt.total_seconds()/60)
+sleep_data_by_day[['time_in_bed','total_time_asleep']].plot(use_index=True)
 plt.show()
 
 
@@ -99,6 +105,7 @@ activeEnergy = convert_to_date_time_append_to_df(activeEnergy)
 activeEnergy.sample(5)
 # %%
 activeEnergy_by_date = activeEnergy.groupby(["start_date"])["value"].sum().reset_index(name="activeEnergy")
+activeEnergy_by_date = activeEnergy_by_date.rename(columns={"start_date": "date"})
 print(activeEnergy_by_date.head())
 print(activeEnergy_by_date.shape)
 
@@ -108,6 +115,7 @@ basalEnergy = convert_to_date_time_append_to_df(basalEnergy)
 basalEnergy.sample(5)
 # %%
 basalEnergy_by_date = basalEnergy.groupby(["start_date"])["value"].sum().reset_index(name="basalEnergy")
+basalEnergy_by_date = basalEnergy_by_date.rename(columns={"start_date": "date"})
 print(basalEnergy_by_date.head())
 print(basalEnergy_by_date.shape)
 
@@ -122,13 +130,25 @@ respiratoryRate_by_date = respiratoryRate.groupby("creation_date").agg(
     respiratoryRate_max=("value", "max"),
     respiratoryRate_min=("value", "min")
 ).reset_index()
-
+respiratoryRate_by_date = respiratoryRate_by_date.rename(columns={"creation_date": "date"})
 print(respiratoryRate_by_date.head())
 print(respiratoryRate_by_date.shape)
 # %%
-respiratoryRate_by_date.set_index("creation_date")[["respiratoryRate_mean", "respiratoryRate_max", "respiratoryRate_min"]].plot()
+respiratoryRate_by_date.set_index("date")[["respiratoryRate_mean", "respiratoryRate_max", "respiratoryRate_min"]].plot()
 
 
 # %%
+df = pd.merge(steps_by_date, restingHR_by_day, on="date", how="outer")
+df = pd.merge(df, vo2max_by_day, on="date", how="outer")
+df = pd.merge(df, sleep_data_by_day, on="date", how="outer")
+df = pd.merge(df, activeEnergy_by_date, on="date", how="outer")
+df = pd.merge(df, basalEnergy_by_date, on="date", how="outer")
+df = pd.merge(df, respiratoryRate_by_date, on="date", how="outer")
+df.head(10)
 
 
+# %%
+df.to_csv("data/output/apple_data.csv")
+
+
+# %%
